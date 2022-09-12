878ea32b57a03a858e4276f5c3a291bc645ced5b
Ethan <ethanbaird94@gmail.com>
2022-09-12 15:03:10 +0100

    adds Visitor test canVisitAttraction()

--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,9 +1,11 @@
 package people;
 
+import attractions.RollerCoaster;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class VisitorTest {
 
@@ -11,7 +13,7 @@ public class VisitorTest {
 
     @Before
     public void before(){
-        visitor = new Visitor(14, 1.2, 40.0);
+        visitor = new Visitor(14, 120, 40.0);
     }
 
     @Test
@@ -21,11 +23,27 @@ public class VisitorTest {
 
     @Test
     public void hasHeight() {
-        assertEquals(1.2, visitor.getHeight(), 0.1);
+        assertEquals(200, visitor.getHeight(), 0.1);
     }
 
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
     }
+
+
+    @Test
+    public void canVisitAttraction(){
+        RollerCoaster rollerCoaster = new RollerCoaster("Big Dipper", 5);
+        visitor.visitAttraction(rollerCoaster);
+        boolean actualAllowed = rollerCoaster.isAllowedTo(visitor);
+        double actualMoney = visitor.getMoney();
+        assertTrue(actualAllowed);
+        assertEquals(31.6, actualMoney, 0.0);
+
+    }
+//    @Test
+//    public void chargesDoubleForOver200Tall(){
+//        Visitor over200 = new Visitor(18,210,20);
+//    }
 }
