2aa310bb4cc5f493cc830505452b4081b14168e5
Ethan <ethanbaird94@gmail.com>
2022-09-12 15:40:38 +0100

    adds ThemePark getAllReviewed()

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,23 @@
+import attractions.Attraction;
+import interfaces.IReview;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+
+    public ThemePark(ArrayList<Attraction> attractions, ArrayList<Stall> stalls) {
+        this.attractions = attractions;
+        this.stalls = stalls;
+    }
+
+    public ArrayList<IReview> getAllReviewed(){
+        ArrayList<IReview> allReviewed = new ArrayList<>();
+        allReviewed.addAll(attractions);
+        allReviewed.addAll(stalls);
+        return allReviewed;
+    }
 }
